10a98ee4c10be0427064ffc213cf8b6e939fc018
Alex <alejandro.fuentesgzm@gmail.com>
2024-03-08 11:10:04 -0600

    Agregando El hook de react para controlar formularios

--- src/auth/Components/FormComponent.jsx
+++ src/auth/Components/FormComponent.jsx
@@ -1,27 +1,13 @@
-import { useEffect } from "react";
-import { useForm } from "../Hooks/useForm";
 import { ButtonComponent, InputComponent } from "../../layout/Components";
 
 export const FormComponent = () => {
-  const { formState, onInputChange } = useForm({
-    email: "",
-    password: "",
-  });
-
-  const { email, password } = formState;
-
-  useEffect(() => {}, []);
-  useEffect(() => {}, [formState]);
-
   return (
     <>
       <form className="w-full px-10" action="">
         <div className="flex flex-col text-textColor my-3">
           <label className="text-lg mb-1 pl-2 text-4 font-light">Email</label>
           <InputComponent
-            onChange={onInputChange}
             type="email"
-            value={email}
             name="email"
             placeholder="name@company.com"
           />
@@ -34,8 +20,6 @@ export const FormComponent = () => {
             type="password"
             name={"password"}
             placeholder={"*****"}
-            value={password}
-            onChange={onInputChange}
           />
         </div>
         <div className="flex flex-col items-start px-3 text-textColor my-3 mt-10">
